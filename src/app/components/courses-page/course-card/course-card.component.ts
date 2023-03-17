3c030ae8b4f06e5c68c599d061244f5ba5a229f3
Sasha Gercen <privetpatrick@gmail.com>
2023-03-17 13:36:49 +0200

    add no video to cards

--- src/app/components/courses-page/course-card/course-card.component.ts
+++ src/app/components/courses-page/course-card/course-card.component.ts
@@ -7,6 +7,7 @@ import {
 } from '@angular/core';
 
 import Hls from 'hls.js';
+import { Observable, of } from 'rxjs';
 import { IPreviewCourse } from 'src/app/interfaces/preview-course.interface';
 
 @Component({
@@ -26,21 +27,30 @@ export class CourseCardComponent {
   }
   @HostListener('mouseleave') mouseleave(e: Event) {
     this.mouseOver = false;
-    if (!this.videoElement) return;
+    if (!this.videoElement || this.noVideo) return;
     this.videoElement.pause();
     this.videoElement.src = '';
   }
 
   mouseOver = false;
+  noVideo = false;
+  noVideo$: Observable<boolean> = of(false);
 
   videoElement!: HTMLVideoElement;
 
   createVideo() {
+    if (this.noVideo) return;
     if (!this.course.meta.courseVideoPreview.link) return;
     this.videoElement = this.video?.nativeElement;
     var hls = new Hls();
     hls.loadSource(this.course.meta.courseVideoPreview.link);
     hls.attachMedia(this.videoElement);
+    hls.on(Hls.Events.ERROR, (event, data) => {
+      if (data.type === 'networkError') {
+        this.noVideo = true;
+        this.noVideo$ = of(true);
+      }
+    });
     hls.on(Hls.Events.MANIFEST_PARSED, () => {
       this.videoElement.muted = true;
       this.videoElement
